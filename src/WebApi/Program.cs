d596e9b2996034eff2b0793faf5714bea9c1494e
seungyongshim <seungyongshim@gmail.com>
2025-02-07 00:10:10 +0900

    OpenAI 클라이언트 설정 및 비밀 관리 개선
    `Program.cs`에서 OpenAI 클라이언트를 설정하는 방식을 수정하여 API 키와 URL을 IConfiguration에서 가져오도록 변경했습니다. OpenAIClient의 생성자에 ApiKeyCredential과 OpenAIClientOptions를 사용하여 클라이언트를 초기화하도록 개선했습니다. 또한, `WebApi.csproj`에 UserSecretsId를 추가하여 사용자 비밀을 안전하게 관리할 수 있도록 했습니다.

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,12 +1,22 @@
+using System.ClientModel;
 using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
 using OpenAI;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddUserSecrets<Program>();
+
 // https://www.nuget.org/packages/Microsoft.Extensions.AI.OpenAI/
 builder.Services.AddChatClient(sp =>
 {
-    var config = sp.GetRequiredService<IConfiguration>();
-    return new OpenAIClient("APIKEY").AsChatClient("gpt-4o-mini");
+    var apikey = sp.GetRequiredService<IConfiguration>().GetValue<string>("OpenAI:ApiKey");
+    var url = sp.GetRequiredService<IConfiguration>().GetValue<string>("OpenAI:ApiUrl");
+
+    return new OpenAIClient(new ApiKeyCredential(apikey)
+    , new OpenAIClientOptions()
+    {
+        Endpoint = new(url)
+    }) .AsChatClient("gpt-4o-mini");
 });
 
 var app = builder.Build();
