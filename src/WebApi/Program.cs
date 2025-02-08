736ebc5e13f905b248d26ab4c5f7d41eb692a891
seungyongshim <seungyongshim@gmail.com>
2025-02-08 14:57:41 +0900

    Azure OpenAI 클라이언트로 전환 및 엔드포인트 추가
    `Program.cs`에서 OpenAI 클라이언트를 Azure OpenAI 클라이언트로 변경하고, 새로운 API 키와 URL을 사용하여 채팅 클라이언트를 설정하는 로직을 추가했습니다. `/chat/{message}` 엔드포인트를 추가하여 클라이언트가 메시지를 전송하고 응답을 받을 수 있도록 했습니다.
    
    `launchSettings.json`에서 환경 변수를 "Development"에서 "Local"로 변경하고, JSON 구조를 수정했습니다.
    
    `WebApi.csproj`에 Azure.AI.OpenAI 및 Microsoft.VisualStudio.Azure.Containers.Tools.Targets 패키지를 추가하여 Azure OpenAI와 Docker 지원을 강화했습니다.
    
    `WebApi.http`에서 API 호출을 `/weatherforecast`에서 `/chat/{message}`로 변경했습니다.
    
    `.dockerignore`에 여러 항목을 추가하여 Docker 빌드 시 무시할 파일 및 디렉토리를 지정했습니다.
    
    `Dockerfile`에 여러 스테이지를 추가하여 ASP.NET Core 애플리케이션을 빌드하고 배포하는 방법을 정의했습니다. 디버그 및 프로덕션 환경을 위한 설정이 포함되어 있습니다.

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,23 +1,38 @@
 using System.ClientModel;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.AI;
-using Microsoft.Extensions.Configuration;
-using OpenAI;
+using ChatMessage = Microsoft.Extensions.AI.ChatMessage;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddUserSecrets<Program>();
 
-// https://www.nuget.org/packages/Microsoft.Extensions.AI.OpenAI/
 builder.Services.AddChatClient(sp =>
 {
-    var apikey = sp.GetRequiredService<IConfiguration>().GetValue<string>("OpenAI:ApiKey");
-    var url = sp.GetRequiredService<IConfiguration>().GetValue<string>("OpenAI:ApiUrl");
+    var azure_ai_key = sp.GetRequiredService<IConfiguration>().GetValue<string>("OpenAI:ApiKey");
+    var azure_ai_url = sp.GetRequiredService<IConfiguration>().GetValue<string>("OpenAI:ApiUrl");
 
-    return new OpenAIClient(new ApiKeyCredential(apikey)
-    , new OpenAIClientOptions()
-    {
-        Endpoint = new(url)
-    }) .AsChatClient("gpt-4o-mini");
+    return new Azure.AI.OpenAI.AzureOpenAIClient
+    (
+        new Uri(azure_ai_url),
+        new ApiKeyCredential(azure_ai_key)
+    ).AsChatClient("gpt-4o");
 });
 
 var app = builder.Build();
+
+
+app.MapGet("/chat/{message}", async (HttpContext context, string message) =>
+{
+    var chatClient = context.RequestServices.GetRequiredService<IChatClient>();
+
+    List<ChatMessage> chatMessages = [];
+
+    chatMessages.Add(new ChatMessage(ChatRole.System, "친절하게 한글로 답변합니다."));
+    chatMessages.Add(new ChatMessage(ChatRole.User, message));
+
+    var response = await chatClient.CompleteAsync(chatMessages);
+
+    return Results.Ok(response);
+});
+
 app.Run();
